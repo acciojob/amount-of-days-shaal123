19b555d5befd3beb3c2a0a648494718aed998bfb
Shalini  V <105047114+shaal123@users.noreply.github.com>
2026-01-03 16:01:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+    // Leap year logic
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+        return 366;
+    }
+    return 365;
+}
+
+// do not change the code below
+let year = prompt("Enter a year:");
+alert(daysOfAYear(Number(year)));
+
